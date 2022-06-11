f050e450f3b9d4e5d3575b18d0f29d884095c849
Denis Zagoruev <denis.zagoruev@gmail.com>
2022-06-11 15:00:55 +0300

    Install Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Feed Engine Frontend Project</h1>
+  <h1 class="text-3xl font-bold">Feed Engine Frontend Project</h1>
 </template>
 
 <style></style>
