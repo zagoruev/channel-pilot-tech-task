f270189bd567a5f30c4ec4f06223c6c425e4edea
Denis Zagoruev <denis.zagoruev@gmail.com>
2022-06-12 13:44:43 +0300

    Install Konva to visualize connections

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import VueKonva from "vue-konva";
+
 import "./assets/base.css";
 
 const app = createApp(App);
 
+app.use(VueKonva);
 app.use(createPinia());
 
 app.mount("#app");
